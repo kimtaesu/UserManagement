c4479ab6c99d8f88f183f2474e454d6e4183eee1
taesu <kimtaesoo188@gmail.com>
2017-03-28 13:15:52 +0900

    init data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,43 @@
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test1@testtest.com', 'test1', 'test');
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test2@testtest.com', 'test2', 'test');
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test3@testtest.com', 'test3', 'test');
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test4@testtest.com', 'test4', 'test');
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test5@testtest.com', 'test5', 'test');
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test6@testtest.com', 'test6', 'test');
+
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test7@testtest.com', 'test7', 'test');
+
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test8@testtest.com', 'test8', 'test');
+
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test9@testtest.com', 'test9', 'test');
+
+
+INSERT INTO account (account_id, user_email, user_name, password)
+VALUES
+  (NULL, 'test10@testtest.com', 'test10', 'test');
